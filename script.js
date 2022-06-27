825ba1f1d32b94681ceed74c319c6dd1ebd849df
Gabriela Chiquetto <gabrielachiquetto@gmail.com>
2022-06-27 19:40:06 -0300

    btn effect added

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 const btn = document.querySelector('.btn');
 const nav = document.querySelector('.nav');
-const listItem = document.querySelectorAll('.nav__link');
 
-btn.addEventListener('click', e => {
-    nav.classList.toggle('nav--active', !nav.classList.contains('nav--active'));
-    //list.classList.toggle('hidden', !list.classList.contains('hidden'));
-    // btn.classList.toggle('btn--active', !btn.classList.contains('btn--active'));
-    // listItem.forEach(item => {
-    //     //item.classList.toggle('hidden', !item.classList.contains('hidden'));
-    //     item.classList.toggle('nav__link--active', !item.classList.contains('nav__link--active'));
-    // });
-});
+btn.addEventListener('click', () => 
+    nav.classList.toggle('nav--active', !nav.classList.contains('nav--active'))
+
+);
