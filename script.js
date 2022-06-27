85f812a5562bd0a7c36dfd41ff3c66a2726cd7c6
Gabriela Chiquetto <gabrielachiquetto@gmail.com>
2022-06-27 17:22:41 -0300

    added some features to css

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const nav = document.querySelector('.nav');
 const listItem = document.querySelectorAll('.nav__link');
 
 btn.addEventListener('click', e => {
-    nav.classList.toggle('nav--hidden', !nav.classList.contains('nav--hidden'));
+    nav.classList.toggle('nav--active', !nav.classList.contains('nav--active'));
     //list.classList.toggle('hidden', !list.classList.contains('hidden'));
-    btn.classList.toggle('btn--hidden', !btn.classList.contains('btn--hidden'));
-    listItem.forEach(item => {
-        //item.classList.toggle('hidden', !item.classList.contains('hidden'));
-        item.classList.toggle('nav__link--hidden', !item.classList.contains('nav__link--hidden'));
-    });
+    // btn.classList.toggle('btn--active', !btn.classList.contains('btn--active'));
+    // listItem.forEach(item => {
+    //     //item.classList.toggle('hidden', !item.classList.contains('hidden'));
+    //     item.classList.toggle('nav__link--active', !item.classList.contains('nav__link--active'));
+    // });
 });
